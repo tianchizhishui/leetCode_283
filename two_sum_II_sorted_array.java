9862779dd50f7b8d5052066e6c2ceb5119cfbea0
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2019-10-16 19:03:46 -0700

    leetcode 167

---
+++ two_sum_II_sorted_array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int[] res = new int[2];
+        
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                res[0] = left + 1;
+                res[1] = right + 1;
+                break;
+            }
+        }
+        return res;
+    }
+}
